e1cd97ca60c84278e1d3bca6b09f89759c09ff9c
e44alex <e44alex@gmail.com>
2020-03-10 06:24:53 +0300

    created project

---
+++ CW_SITAIRIS/CW_SITAIRIS/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CW_SITAIRIS.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
